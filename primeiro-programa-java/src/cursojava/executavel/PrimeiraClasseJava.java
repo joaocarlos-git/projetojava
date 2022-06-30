f1ad8c216fbb4bf2293b2a2f05c6e272051162b8
JOAO RIBEIRO <jocasiribeiro@hotmail.com>
2022-06-30 16:02:24 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um metodo auto executável em java*/
+ public static void main(String[] args) {
+	 
+	 /*escreve texto no console*/
+	System.out.println("Meu primeiro programa em Java do Curso Fromação Java Web ");
+ }
+}
