64d96b79c2f574564de7538aa01a63300df433a6
JOAO RIBEIRO <jocasiribeiro@hotmail.com>
2022-07-01 08:46:06 -0300

    asdfer

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	/*Main é um metodo auto executável em javaasdfasfassdfasdfasdf*/
+	/*Main é um metodo sdfasdfauto executável em javaasdfasfassdfasdfasdf*/
  public static void main(String[] args) {
 	 
 	 /*escreve texto no consolennnnnnadfasfasfsdasdfasfsafsafasdsdfasdfasdf*/
